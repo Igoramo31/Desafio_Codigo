a3faf1d21e9f525b8df77a68995223d5b268d527
Igor Ramos <138816047+Igoramo31@users.noreply.github.com>
2023-07-06 18:17:19 -0300

    Update PwC2.1.c

--- PwC2.c
+++ PwC2.c
@@ -2,6 +2,20 @@
 #include <string.h>
 
 void RemovaDuplicado(char str[]) {
+  int tamanho = strlen(str);
+  int i, j, k;
+
+  for (i = 0; i < tamanho - 1; i++) {
+      for (j = i + 1; j < tamanho; j++) {
+        if (str[i] == str[j]) {
+          for (k = j; k < tamanho - 1; k++) {
+            str[k] = str[k + 1];
+          }
+          tamanho--;
+          j--;
+        }
+      }
+  }
 
 }
 
